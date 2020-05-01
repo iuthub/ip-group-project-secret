c5902cc953a3c8a734ed8825b94f23b23379a2d5
azNodirbek <azizovnodirbek1999@gmail.com>
2020-05-01 09:40:53 +0500

    contact page is created

---
+++ contact-us.php
@@ -0,0 +1,37 @@
+</header><!--/header-->
+
+        <!-- Page Title -->
+		<div class="section section-breadcrumbs">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-6">
+						<h1>Contact Us</h1>
+					</div>
+				</div>
+			</div>
+		</div>
+
+        <div class="section">
+	    	<div class="container">
+	        	<div class="row">
+					<h3>Get in Touch with Us</h3>
+					</hr>
+	        		<div class="col-sm-6">
+	        			<!-- Contact Info -->
+	        			<p class="contact-us-details">
+	        				<b>Address:</b> 9, Ziyolilar street, Mirzo Ulugbek district, Tashkent, Uzbekistan<br/>
+	        				<b>Phone:</b> +998998803996<br/>
+	        				<b>Fax: </b> no exit yet<br/>
+	        				<b>Email:</b> <a href="n.azizov@student.inha.uz">n.azizov@student.inha.uz</a>
+	        			</p>
+	        			<!-- End Contact Info -->
+	        		</div>
+	        		<div class="col-sm-6">
+
+	        		</div>
+	        	</div>
+	    	</div>
+	    </div>
+
+    </body>
+</html>
