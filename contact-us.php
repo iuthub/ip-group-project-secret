0678471e464144152fd5326fc12f9d717e0d1116
azNodirbek <azizovnodirbek1999@gmail.com>
2020-05-01 10:52:09 +0500

    map is added

--- contact-us.php
+++ contact-us.php
@@ -1,5 +1,5 @@
-</header><!--/header-->
 
+</header><!--/header-->
         <!-- Page Title -->
 		<div class="section section-breadcrumbs">
 			<div class="container">
@@ -10,7 +10,21 @@
 				</div>
 			</div>
 		</div>
-
+		<div class="section section-map">
+<div class="col-sm-12" style="padding:0;">
+	<!-- Map -->
+	<div id="contact-us-map">
+		<!-- <iframe width="100%" height="100%" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;q=Twitter,+Inc.,+Market+Street,+San+Francisco,+CA&amp;aq=0&amp;oq=twitter&amp;sll=28.659344,-81.187888&amp;sspn=0.128789,0.264187&amp;ie=UTF8&amp;hq=Twitter,+Inc.,+Market+Street,+San+Francisco,+CA&amp;t=m&amp;z=15&amp;iwloc=A&amp;output=embed"></iframe> -->
+		
+	<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d16946.130150796063!2d69.33293751271592!3d41.33755068604025!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x3772abeffc72e7b8!2sInha+University+in+Tashkent!5e0!3m2!1sen!2s!4v1494154023868" width="1685" height="440" frameborder="0" style="border:0" allowfullscreen></iframe>
+		<br />
+		<small>
+			
+		</small>
+		</iframe>
+	</div>
+	<!-- End Map -->
+</div>
         <div class="section">
 	    	<div class="container">
 	        	<div class="row">
@@ -32,6 +46,5 @@
 	        	</div>
 	    	</div>
 	    </div>
-
     </body>
 </html>
